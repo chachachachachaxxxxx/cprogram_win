f6299cfb2c1fd726900f800b6d88a9346f3325e3
chachachachachaxxxxx <205373694@qq.com>
2021-10-09 08:32:14 +0800

    202110090832

---
+++ 问题求解与程序设计/Lab02/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argv, char *argc[])
+{
+    ifstream in(argc[1]);
+    
+}
---
+++ 问题求解与程序设计/Lab02/myHash.h
@@ -0,0 +1,8 @@
+#ifndef MYHASH_H
+#define MYHASH_H
+
+typedef struct node{
+    
+}
+
+#endif
